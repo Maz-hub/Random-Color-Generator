380b79c120f03d9190d3528129e497e8b4793da7
Maz <mazoolander@me.com>
2025-07-27 09:53:14 +0200

    Implement random HEX color generation functionality

--- src/random-color/index.jsx
+++ src/random-color/index.jsx
@@ -5,7 +5,17 @@ export default function RandomColor() {
   const [typeOfColor, setTypeOfColor] = useState("hex");
   const [color, setColor] = useState("#000000");
 
-  function handleCreateRandomHexColor() {}
+  function handleCreateRandomHexColor() {
+    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[Math.floor(Math.random() * hex.length)];
+    }
+    //console.log(hexColor);
+    setColor(hexColor);
+  }
 
   function handleCreateRandomRgbColor() {}
 
