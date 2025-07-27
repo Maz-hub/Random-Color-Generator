363d9472b8ab8315a2b8cdfbbb35817b2cc9685e
Maz <mazoolander@me.com>
2025-07-27 09:37:50 +0200

    set color generation functionality with event handlers for HEX and RGB colors

--- src/random-color/index.jsx
+++ src/random-color/index.jsx
@@ -5,12 +5,29 @@ export default function RandomColor() {
   const [typeOfColor, setTypeOfColor] = useState("hex");
   const [color, setColor] = useState("#000000");
 
+  function handleCreateRandomHexColor() {}
+
+  function handleCreateRandomRgbColor() {}
+
   return (
     <div style={{ width: "100vw", height: "100vh", background: color }}>
       <div className="buttons">
-        <button className="btn">Create HEX Color</button>
-        <button className="btn">Create RGB Color</button>
-        <button className="btn">Generate Random Color</button>
+        <button onClick={() => setTypeOfColor("hex")} className="btn">
+          Create HEX Color
+        </button>
+        <button onClick={() => setTypeOfColor("rgb")} className="btn">
+          Create RGB Color
+        </button>
+        <button
+          onClick={
+            typeOfColor === "hex"
+              ? handleCreateRandomHexColor
+              : handleCreateRandomRgbColor
+          }
+          className="btn"
+        >
+          Generate Random Color
+        </button>
       </div>
     </div>
   );
